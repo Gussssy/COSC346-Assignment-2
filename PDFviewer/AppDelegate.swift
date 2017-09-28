908f3b3d72e0c6c34c5cbaca80281a1e00a36e19
Tristan Gardner <trgardner@oucs1528.otago.ac.nz>
2017-09-28 19:56:35 +1300

    Initial Commit

---
+++ PDFviewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PDFviewer
+//
+//  Created by Tristan Gardner on 9/28/17.
+//  Copyright © 2017 Tristan Gardner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PDFviewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PDFviewer
+//
+//  Created by Tristan Gardner on 9/28/17.
+//  Copyright © 2017 Tristan Gardner. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
