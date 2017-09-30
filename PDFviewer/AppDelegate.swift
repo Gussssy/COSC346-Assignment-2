c2a9c2413400c1799af1f1ec3706f6a486ad8c60
Tristan Gardner <trgardner@oucs1533.otago.ac.nz>
2017-09-30 15:49:38 +1300

    Trying again

--- PDFviewer/AppDelegate.swift
+++ PDFviewer/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import Cocoa
+import Quartz
+
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
@@ -15,12 +17,56 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        //let url = NSURL.fileURL(withPath: "http://www.cs.otago.ac.nz/cosc346/Lectures/Lecture1.pdf")
+        //let pdf = PDFDocument(url: url)
+        screen.allowsDragging = true
+        //screen.document = pdf
+        document = screen.document
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+    @IBOutlet weak var window: NSWindow!
 
+    @IBOutlet weak var screen: PDFView!
+    
+    @IBOutlet weak var thumbnail: PDFThumbnailView!
+
+    @IBOutlet weak var nextPageButton: NSButton!
+
+    @IBOutlet weak var lastPageButton: NSButton!
+
+    @IBOutlet weak var nextLectureButton: NSButton!
+    
+    @IBOutlet weak var lastLectureButton: NSButton!
+    
+    var document: PDFDocument?
+    
+    @IBAction func nextPage(_ sender: Any) {
+        if screen.canGoToNextPage(){
+            screen.goToNextPage(_ : Any)
+        }
+    }
+    
+    @IBAction func previousPage(_ sender: Any) {
+        if screen.canGoToPreviousPage(){
+            screen.goToPreviousPage(_ : Any)
+        }
+
+    }
+    
+    @IBOutlet weak var pageNumberEntry: NSTextField!
+    @IBOutlet weak var jumpButton: NSButton!
+    
+    @IBAction func jumpToPage(_ sender: Any) {
+        let num = Int(pageNumberEntry.imageTitle())
+        if  num! >= (document!.pageCount){
+            screen.go(to: document!.page(at: num!)!)
+        }
+    }
+    
+    
 }
 
